b8e18eeb397cd5bbbd861a77e6cde8a878d4122a
beo <c.behounek+github@gmail.com>
2012-12-26 10:50:28 +0100

    Initial

---
+++ TeMa/grails-app/conf/BootStrap.groovy
@@ -0,0 +1,17 @@
+import de.elementEvents.tema.user.Role;
+import de.elementEvents.tema.user.User;
+import de.elementEvents.tema.user.UserRole;
+
+class BootStrap { def springSecurityService
+
+    def init = { servletContext ->
+		def userRole = Role.findByAuthority('ROLE_USER') ?: new Role(authority: 'ROLE_USER').save(failOnError: true) 
+		def adminRole = Role.findByAuthority('ROLE_ADMIN') ?: new Role(authority: 'ROLE_ADMIN').save(failOnError: true)
+		
+		def adminUser = User.findByUsername('admin') ?: new User( username: 'admin', password: springSecurityService.encodePassword('admin'), enabled: true).save(failOnError: true)
+		
+		if (!adminUser.authorities.contains(adminRole)) { UserRole.create adminUser, adminRole}
+    }
+    def destroy = {
+    }
+}
